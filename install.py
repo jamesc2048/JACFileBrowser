c8b039b957143bcf6d7f5dcf40f8b75dbc7e9252
James Crisafulli <james2048@gmail.com>
2023-09-12 00:55:06 +0100

    End to end windows build

--- install.py
+++ install.py
@@ -4,7 +4,7 @@ import subprocess as sp
 from path import Path
 
 install_dir = Path("JACFileBrowser-build-0.1")
-qt_sdk_path = r"C:\SDK\Qt\6.5.0\msvc2019_64"
+qt_sdk_path = r"C:\SDK\Qt\6.6.0\msvc2019_64"
 
 # read env var Qt6_DIR for Github Actions
 build_dir = Path("build-JACFileBrowser")
@@ -12,22 +12,13 @@ build_dir.rmtree(ignore_errors=True)
 build_dir.mkdir()
 build_dir.chdir()
 
-sp.check_call(r""""C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat" cmake -G Ninja -DCMAKE_MAKE_PROGRAM=C:\SDK\Qt\Tools\Ninja\ninja.exe ..\JACFileBrowser\CMakeLists.txt -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=C:\SDK\Qt\6.5.0\msvc2019_64\lib\cmake""", shell=True)
+sp.check_call(rf""""C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat" & cmake -G Ninja -DCMAKE_MAKE_PROGRAM=C:\SDK\Qt\Tools\Ninja\ninja.exe ..\JACFileBrowser\CMakeLists.txt -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH={qt_sdk_path}\lib\cmake -DCMAKE_INSTALL_PREFIX={install_dir}""", shell=True)
 
+sp.check_call(r""""C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat" & cmake --build .""", shell=True)
+sp.check_call(r"""cmake --install .""", shell=True)
 
-install_dir.rmtree(ignore_errors=True)
-install_dir.mkdir()
-
-for f in (x for x in build_dir.listdir() if x.ext in [".dll", ".exe"]):
-    print(f)
-
-    if f.basename() == "appJACFileBrowser.exe":
-        shutil.copy(build_dir / f.basename(), install_dir / "JACFileBrowser.exe")
-    else:
-        shutil.copy(build_dir / f.basename(), install_dir)
-
-sp.check_call([Path(qt_sdk_path) / "bin" / "windeployqt.exe", 
-            install_dir / "JACFileBrowser.exe", 
+sp.check_call([Path(qt_sdk_path) / "bin" / "windeployqt6.exe", 
+            install_dir / "bin" / "appJACFileBrowser.exe", 
             "--qmldir", 
             "JACFileBrowser",
             "--no-opengl-sw",
